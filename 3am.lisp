c8718dd8d2997510b748a7668229f3a44a5a83ed
Matteo Landi <matteo@matteolandi.net>
2023-04-17 09:11:33 +0200

    Rename EVAL/TEST back to TEST
    If users want to opt out of this "define and run" behavior, they can
    always directly use 1am instead.

--- 3am.lisp
+++ 3am.lisp
@@ -1,15 +1,15 @@
 (defpackage #:3am
   (:use #:cl)
-  (:import-from #:1am #:test #:is #:signals #:run #:*tests*)
-  (:export #:test #:is #:signals #:run #:*tests* #:eval/test))
+  (:import-from #:1am #:is #:signals #:run #:*tests*)
+  (:export #:test #:is #:signals #:run #:*tests*))
 (in-package #:3am)
 
-(defmacro eval/test (name &body body)
+(defmacro test (name &body body)
   "Define a test function, add it to `*tests*' and then run it immediately.
 
   Note: the test is immediately run only if the macro is directly evaluated in
   the REPL, i.e., if neither `*compile-file-pathname*' nor `*load-pathname*'
   are set."
-  `(prog1 (test ,name ,@body)
+  `(prog1 (1am:test ,name ,@body)
      (unless (or *compile-file-pathname* *load-pathname*)
        (,name))))
